aa05863e77f320d333292450469407f15004eb76
Magooch <crvallecito@gmail.com>
2025-02-22 06:49:08 -0700

    Set scoreboard and point system variables

---
+++ scoreboard/app.js
@@ -0,0 +1,11 @@
+let homeScore = document.getElementById('home-score')
+let guestScore = document.getElementById('guest-score')
+
+let homeOnePoint = document.getElementById('home-one')
+let homeTwoPoint = document.getElementById('home-two')
+let homeThreePoint = document.getElementById('home-three')
+
+let guestOnePoint = document.getElementById('guest-one')
+let guestTwoPoint = document.getElementById('guest-two')
+let guestThreePoint = document.getElementById('guest-three')
+
