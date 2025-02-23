b912e32b8f3a1c536db8d4dff0a029d3267929ca
Magooch <crvallecito@gmail.com>
2025-02-23 13:09:22 -0700

    Add start timer and reset timer functions

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,34 +1,58 @@
 // Home/Guest Point Values
 let homeScore = document.getElementById('home-score')
-let homeValue = homeScore.innerText;
+let homeValue = homeScore.innerText
 let guestScore = document.getElementById('guest-score')
 
-// Button Calls for Functions and Point Counter (Home)
+// Buttons for Point Counters (Home)
 let homeOnePoint = document.getElementById('home-one')
 let homeTwoPoint = document.getElementById('home-two')
 let homeThreePoint = document.getElementById('home-three')
 let homeCount = 0
 
-// Button Calls for Functions and Point Counter (Guest)
+// Buttons for Point Counter (Guest)
 let guestOnePoint = document.getElementById('guest-one')
 let guestTwoPoint = document.getElementById('guest-two')
 let guestThreePoint = document.getElementById('guest-three')
 let guestCount = 0
 
+// Period Counter
+let period = document.getElementById('period')
+let periodVal = period.innerText
+let periodCount = 0
+
+// Reset Button
+let reset = document.getElementById('reset-timer')
+
 // Timer Variables and Function
 let time = document.getElementById('timer')
-let minutes = document.getElementById('minute')
-let seconds = document.getElementById('second')
-let secondsVariable = 59
-let secondVariable = seconds.innerHTML
-
 let startTimer = document.getElementById('start-timer')
 
-function timer() {
-    secondVariable--;
-    while (secondVariable > 0) {
-        seconds.innerHTML = secondsVariable
-        seconds.textContent = secondVariable
+let minutes = document.getElementById('minute')
+let minutesVal = minutes.innerText
+let minutesVar = 2
+let secondZero = 0
+
+let seconds = document.getElementById('second')
+let secondsVal = seconds.innerText
+let secondsVar = 59
+
+
+// Starts Timer and Increments Period
+function start() {
+    for (let i = 0; i < 2; i++) {
+        while (secondsVar >= 0) {
+            seconds.textContent = secondsVar
+            if (secondsVar < 10 || secondsVar > 0) {
+                seconds.textContent = `${secondZero}` + `${secondsVar}`
+            }
+            secondsVar--
+        }
+        minutesVar -= 1
+        minutes.textContent = minutesVar
+        if (periodCount < 2) {
+            periodCount += 1
+            period.textContent = periodCount
+        }
     }
 }
 
@@ -74,6 +98,12 @@ function threePointGuest() {
     leader()
 }
 
+// Resets timer
+function resetTimer() {
+    minutes.textContent = minutesVal
+    seconds.textContent = secondsVal
+}
+
 // Resets Both Scores
 function newGame() {
     homeCount = 0
@@ -82,4 +112,7 @@ function newGame() {
     guestScore.textContent = guestCount
     homeScore.style.color = '#F94F6D'
     guestScore.style.color = '#F94F6D'
+    minutes.textContent = minutesVal
+    seconds.textContent = secondsVal
+    period.textContent = periodVal
 }
