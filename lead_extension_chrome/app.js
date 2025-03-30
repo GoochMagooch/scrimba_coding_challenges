c94efa01163ec2d353e3793e944015ec6c97aef2
Magooch <crvallecito@gmail.com>
2025-03-30 13:43:05 -0600

    Set pseudocode and skeleton code for storing all leads in local storage

--- lead_extension_chrome/app.js
+++ lead_extension_chrome/app.js
@@ -21,3 +21,22 @@ ulEl.innerHTML = storageArrayItems
 }
 
 renderLeads()
+
+// Stores old leads
+/*
+let allLeads = ''
+function oldLeads() {
+    allLeads = JSON.parse(localStorage.getItem("myLeadsStorage")) || []
+    myLeads.push(storageArray)
+    localStorage.setItem("myLeadsStorage", JSON.stringify(myLeads))
+}
+
+oldLeads()
+*/
+
+// What I think I have to do:
+// Take current local storage (item 1)
+// Store as second local storage item (item 2)
+// Upon document reload both items will store original list of leads
+// Item 1 will be cleared, with the event listener, item 2 will be appended with new leads
+// Upon next reload, item 1 will contain previous leads, item 2 will contain all leads up to that point
